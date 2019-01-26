3f6e127afc6328f35eb52a184cebaeee4e30a6b1
David Thor <david.thor@arcussolutions.com>
2019-01-25 19:01:38 -0500

    Finished python example service

--- python-subtraction-service/main.py
+++ python-subtraction-service/main.py
@@ -8,4 +8,7 @@ class Architect:
     def Subtract(self, request, context):
         first = request.first
         second = request.second * -1
-        return first
+
+        add_request = self.addition_service['messages'].AddRequest(first=first, second=second)
+        add_response = self.addition_service['client'].Add(add_request)
+        return self.subtraction_service['messages'].SubtractionResponse(output=add_response.output)
