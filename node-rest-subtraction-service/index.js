8653027dd618dcc987134bcc9634bf1f9486d566
David Thor <david.thor@arcussolutions.com>
2019-07-18 13:28:21 -0400

    Added sample REST subtraction service

---
+++ node-rest-subtraction-service/index.js
@@ -0,0 +1,38 @@
+const grpc = require('grpc');
+const architect = require('architect-sdk').default;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+architect.useGRPC(grpc);
+const addition_service = architect.service('addition-service');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/subtract', (req, res) => {
+  let { first, second } = req.query;
+  second *= -1;
+
+  const addition_request = new addition_service.definitions.AddRequest();
+  addition_request.setFirst(first);
+  addition_request.setSecond(second);
+  addition_service.client.add(
+    addition_request,
+    (error, addition_response) => {
+      if (error) {
+        return res.status(500).send(error);
+      } else {
+        return res.status(200).json({
+          result: addition_response.getOutput()
+        });
+      }
+    }
+  );
+});
+
+const { HOST, PORT } = process.env;
+app.listen(PORT, () => {
+  console.log('Listening at %s:%s', HOST, PORT);
+});
