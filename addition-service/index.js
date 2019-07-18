406b23148eb15c4462477646fdcd2f95f4ed5c3d
David Thor <david.thor@arcussolutions.com>
2019-07-18 18:05:32 -0400

    Added postgres datastore to addition-service

--- addition-service/index.js
+++ addition-service/index.js
@@ -1,20 +1,48 @@
 'use strict';
 
 const grpc = require('grpc');
+const Sequelize = require('sequelize');
+const architect = require('architect-sdk').default;
 const { AddResponse } = require('./architect_services/addition_service/service_pb');
 const { ArchitectService } = require('./architect_services/addition_service/service_grpc_pb');
+const initDatabaseModels = require('./db_models');
 
-function add(call, callback) {
-  const add_request = call.request;
-  const response = new AddResponse();
-  response.setOutput(add_request.getFirst() + add_request.getSecond());
-  callback(null, response);
-}
+// Setup DB client
+const primary_db_config = architect.datastore('primary');
+const primary_db_client = new Sequelize('addition_service', primary_db_config.username, primary_db_config.password, {
+  host: primary_db_config.host,
+  port: primary_db_config.port,
+  dialect: 'postgres'
+});
+
+// Setup DB models
+const { RequestLog } = initDatabaseModels(primary_db_client);
+
+// Migrate the DB to match the model
+primary_db_client.sync();
+
+// Setup GRPC Service methods
+const service_impl = {
+
+  add: (call, callback) => {
+    const add_request = call.request;
+    RequestLog.create({
+      first: add_request.getFirst(),
+      second: add_request.getSecond(),
+      result: add_request.getFirst() + add_request.getSecond()
+    }).then(record => {
+      const response = new AddResponse();
+      response.setOutput(record.result);
+      callback(null, response);
+    });
+  }
+
+};
 
 // START SERVER
 const { HOST, PORT } = process.env;
 const server = new grpc.Server();
-server.addService(ArchitectService, { add });
+server.addService(ArchitectService, service_impl);
 server.bind(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure());
 server.start();
 console.log(`Listening at ${HOST}:${PORT}`);
