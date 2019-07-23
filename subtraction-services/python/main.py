bc4ffe1ecd439742e130aeb4cca3c8278ac7c2be
TJ Higgins <tj@architect.io>
2019-07-22 20:51:10 -0400

    Updated python service for sdk

--- subtraction-services/python/main.py
+++ subtraction-services/python/main.py
@@ -1,14 +1,29 @@
-class Architect:
-    dependencies = ['subtraction-service', 'addition-service']
+from concurrent import futures
+import grpc
+import logging
+import os
+import time
 
-    def __init__(self, subtraction_service, addition_service):
-        self.subtraction_service = subtraction_service
-        self.addition_service = addition_service
+from src import architect
 
-    def Subtract(self, request, context):
-        first = request.first
-        second = request.second * -1
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-        add_request = self.addition_service['messages'].AddRequest(first=first, second=second)
-        add_response = self.addition_service['client'].Add(add_request)
-        return self.subtraction_service['messages'].SubtractionResponse(output=add_response.output)
+
+def serve():
+    host = os.environ['HOST']
+    port = os.environ['PORT']
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server.add_insecure_port('{}:{}'.format(host, port))
+    server.add_generic_rpc_handlers()
+    server.start()
+
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
