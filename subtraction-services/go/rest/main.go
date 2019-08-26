1b8ce05dcc4bc0cb0a5c25e100ef09f1592efdd2
Ryan Cahill <ryan.cahill444@gmail.com>
2019-08-26 15:50:11 -0400

    working subtraction service

---
+++ subtraction-services/go/rest/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	// "fmt"
+	"os"
+	"strconv"
+
+	//architect "./architect"
+	architect "github.com/architect-team/go-sdk"
+	echo "github.com/labstack/echo"
+	gjson "github.com/tidwall/gjson"
+)
+
+func main() {
+	// resp := additionService.Client().Get("/add", map[string]string{
+	//      "first": "50",
+	//      "second": "5",
+	// })
+	// datastore := architect.Datastore("primary")
+	// fmt.Println(datastore)
+
+	echoServer := echo.New()
+	echoServer.GET("/subtract", subtract)
+	echoServer.Logger.Fatal(echoServer.Start(os.Getenv("HOST") + ":" + os.Getenv("PORT")))
+}
+
+func subtract(c echo.Context) error {
+	first := c.QueryParam("first")
+	secondInt, _ := strconv.Atoi(c.QueryParam("second"))
+	secondInt = secondInt * -1
+	secondString := strconv.Itoa(secondInt)
+	
+	additionService := architect.Service("architect/addition-service")
+	params := map[string]string{}
+	params["first"] = first
+	params["second"] = secondString
+	response := additionService.Client().Get("/add", params)
+	
+	result := gjson.Get(response.String(), "result")
+	resultInt, _ := strconv.Atoi(result.String())
+	return c.JSON(200, map[string]int{"result": resultInt})
+}
