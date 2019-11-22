3a6d67c6b76826932a0981c77c4d64871a413993
David Thor <david.thor@arcussolutions.com>
2019-11-22 16:11:02 -0500

    WIP: updated services to include contract-based names and removed stubs

--- subtraction-services/node/grpc/src/index.js
+++ subtraction-services/node/grpc/src/index.js
@@ -3,7 +3,7 @@
 const grpc = require('grpc');
 const architect = require('@architect-io/sdk').default;
 
-const addition_service = architect.service('architect/addition-service');
+const addition_service = architect.service('architect/addition-service-grpc');
 const { SubtractionResponse } = architect.current_service().defs;
 
 const subtract = (call, callback) => {
