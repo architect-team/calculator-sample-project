179b6bbd20089efd678061ae06facd156eab2b8f
David Thor <david.thor@arcussolutions.com>
2019-11-23 15:29:21 -0500

    Updated go service to point to rest addition service

--- subtraction-services/go/rest/main.go
+++ subtraction-services/go/rest/main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-    "os"
-    "strconv"
-    sql "database/sql"
+	sql "database/sql"
+	"os"
+	"strconv"
 
-    _ "github.com/lib/pq"
-    architect "github.com/architect-team/go-sdk"
-    echo "github.com/labstack/echo"
-    gjson "github.com/tidwall/gjson"
+	architect "github.com/architect-team/go-sdk"
+	echo "github.com/labstack/echo"
+	_ "github.com/lib/pq"
+	gjson "github.com/tidwall/gjson"
 )
 
 func main() {
-    datastore, err := architect.Datastore("primary")
-    if err != nil {
-        panic(err)
-    }
-    dbName := datastore["name"].(string)
-    user := datastore["username"].(string)
-    password := datastore["password"].(string)
-    db, err := sql.Open("postgres", "user=" + user + "password=" + password + "dbname=" + dbName)
-    if err != nil {
-        panic(err)
-    }
-    db.Close()
+	datastore, err := architect.Datastore("primary")
+	if err != nil {
+		panic(err)
+	}
+	dbName := datastore["name"].(string)
+	user := datastore["username"].(string)
+	password := datastore["password"].(string)
+	db, err := sql.Open("postgres", "user="+user+"password="+password+"dbname="+dbName)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
 
-    echoServer := echo.New()
-    echoServer.GET("/subtract", subtract)
-    echoServer.Logger.Fatal(echoServer.Start(os.Getenv("HOST") + ":" + os.Getenv("PORT")))
+	echoServer := echo.New()
+	echoServer.GET("/subtract", subtract)
+	echoServer.Logger.Fatal(echoServer.Start(os.Getenv("HOST") + ":" + os.Getenv("PORT")))
 }
 
 func subtract(c echo.Context) error {
-    first := c.QueryParam("first")
-    secondInt, err := strconv.Atoi(c.QueryParam("second"))
-    if err != nil {
-        return err
-    }
-    secondInt = secondInt * -1
-    secondString := strconv.Itoa(secondInt)
+	first := c.QueryParam("first")
+	secondInt, err := strconv.Atoi(c.QueryParam("second"))
+	if err != nil {
+		return err
+	}
+	secondInt = secondInt * -1
+	secondString := strconv.Itoa(secondInt)
 
-    additionService, err := architect.Service("architect/addition-service")
-    if err != nil {
-        return err
-    }
-    params := map[string]string{}
-    params["first"] = first
-    params["second"] = secondString
-    response, err := additionService.Client().R().SetQueryParams(params).Get("/add")
-    if err != nil {
-        return err
-    }
+	additionService, err := architect.Service("architect/addition-service-rest")
+	if err != nil {
+		return err
+	}
+	params := map[string]string{}
+	params["first"] = first
+	params["second"] = secondString
+	response, err := additionService.Client().R().SetQueryParams(params).Get("/add")
+	if err != nil {
+		return err
+	}
 
-    result := gjson.Get(response.String(), "result")
-    resultInt, err := strconv.Atoi(result.String())
-    if err != nil {
-        return err
-    }
-    return c.JSON(200, map[string]int{"result": resultInt})
+	result := gjson.Get(response.String(), "result")
+	resultInt, err := strconv.Atoi(result.String())
+	if err != nil {
+		return err
+	}
+	return c.JSON(200, map[string]int{"result": resultInt})
 }
